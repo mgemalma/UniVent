3a358b330cf6ec0d34b6ae9ca03a337a1cf1a162
Andrew Peterson <andrewpeterson@pal-nat186-97-218.itap.purdue.edu>
2017-09-17 19:18:22 -0400

    rebekah is working

--- Pods/Target Support Files/Pods-UniVent/Pods-UniVent-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UniVent : NSObject
-@end
-@implementation PodsDummy_Pods_UniVent
-@end
--- Pods/Target Support Files/Pods-UniVentUITests/Pods-UniVentUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UniVentUITests : NSObject
-@end
-@implementation PodsDummy_Pods_UniVentUITests
-@end
