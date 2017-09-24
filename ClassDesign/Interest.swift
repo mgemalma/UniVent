a9ecac2ff73324f20c84bb906c91e5930b56a454
Amjad Zahraa <azahraa@Amjads-MacBook-Pro.local>
2017-09-23 20:17:47 -0400

    Amjad: Just implemented enumerator.

---
+++ ClassDesign/Interest.swift
@@ -0,0 +1,24 @@
+/**
+ *  Author: Amjad Zahraa
+ *  Description: Interest enumerator for filtering events.
+ **/
+
+enum Interest {
+    // Use this as default or null
+    case None
+    
+    // Got these interest from BoilerLink
+    case Callout
+    case Charity
+    case Community_Service
+    case Concert
+    case Conference
+    case Party
+    case Demonstration
+    case Education
+    case Festival
+    case Meeting
+    case Recreation
+    case Social
+    case Travel
+}
