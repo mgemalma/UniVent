f16b43bfe20c7a0b83aeefcc7db1b039690496cb
Amjad Zahraa <azahraa@Amjads-MacBook-Pro.local>
2017-10-05 22:11:38 -0400

    Amjad: Fixed spaces and single quotes in strings.

--- ClassDesign/AppToDb.swift
+++ ClassDesign/AppToDb.swift
@@ -3,7 +3,10 @@ import UIKit
 
 func insertEvent(event1: Event) {
 
-    let stringURL = "https://gymbuddyapp.net/insertEvent.php?eventID=\(event1.getEventID())&createdTime=\(event1.getTime().getCreatedTime().timeIntervalSince1970)&startTime=\(event1.getTime().getStartTime().timeIntervalSince1970)&endTime=\(event1.getTime().getEndTime().timeIntervalSince1970)&address=\(event1.getLoc().getAddress())&latitude=\(event1.getLoc().getLatitude())&longitude=\(event1.getLoc().getLongitude())&rating=\(event1.getStat().getRatingDouble())&attendanceCount=\(event1.getStat().getHeadCount())&flag=\(event1.getStat().getFlagCount())&ratingCount=\(event1.getStat().getRatingCount())&creatorID=\(event1.getGen().getHostID())&title=\(event1.getGen().getTitle())&description=\(event1.getGen().getDescription())&type=\(event1.getGen().getType().rawValue)"
+    var stringURL = "https://gymbuddyapp.net/insertEvent.php?eventID=\(event1.getEventID())&createdTime=\(event1.getTime().getCreatedTime().timeIntervalSince1970)&startTime=\(event1.getTime().getStartTime().timeIntervalSince1970)&endTime=\(event1.getTime().getEndTime().timeIntervalSince1970)&address=\(event1.getLoc().getAddress())&latitude=\(event1.getLoc().getLatitude())&longitude=\(event1.getLoc().getLongitude())&rating=\(event1.getStat().getRatingDouble())&attendanceCount=\(event1.getStat().getHeadCount())&flag=\(event1.getStat().getFlagCount())&ratingCount=\(event1.getStat().getRatingCount())&creatorID=\(event1.getGen().getHostID())&title=\(event1.getGen().getTitle())&description=\(event1.getGen().getDescription())&type=\(event1.getGen().getType().rawValue)"
+    
+    stringURL = stringURL.replacingOccurrences(of: " ", with: "%20")
+    stringURL = stringURL.replacingOccurrences(of: "'", with: "''")
     
     print(stringURL)
     
@@ -25,7 +28,10 @@ func insertEvent(event1: Event) {
 
 func insertUser(user1: User) {
     
-    let stringURL = "https://gymbuddyapp.net/insertUser.php?userID=\(user1.getUserID())&userName=\(user1.getUserName())&flagCount=\(user1.getUserHistory().getFlagCount())&postedEvents=Sprint2Lol&attendingEvents=Sprint2Too&interests=0&schedule=Sprint2&deviceID=q324as5d6fty"
+    var stringURL = "https://gymbuddyapp.net/insertUser.php?userID=\(user1.getUserID())&userName=\(user1.getUserName())&flagCount=\(user1.getUserHistory().getFlagCount())&postedEvents=Sprint2Lol&attendingEvents=Sprint2Too&interests=0&schedule=Sprint2&deviceID=q324as5d6fty"
+    
+    stringURL = stringURL.replacingOccurrences(of: " ", with: "%20")
+    stringURL = stringURL.replacingOccurrences(of: "'", with: "''")
     
 //        print(stringURL)
     
@@ -56,7 +62,7 @@ func changeEventAttendance(eventID: Int, option: Character) {
         stringURL = "https://gymbuddyapp.net/changeAttendance.php?eventID=\(eventID)&operator=I"
     }
     
-    print(stringURL)
+//    print(stringURL)
     
     let Url = URL(string: stringURL)
     if let url = Url {
@@ -86,7 +92,7 @@ func changeRatingCount(eventID: Int, option: Character) {
         stringURL = "https://gymbuddyapp.net/ratingCount.php?eventID=\(eventID)&operator=I"
     }
     
-    print(stringURL)
+//    print(stringURL)
     
     let Url = URL(string: stringURL)
     if let url = Url {
@@ -115,7 +121,37 @@ func changeFlagCountEvent(eventID: Int, option: Character) {
         stringURL = "https://gymbuddyapp.net/flagCountEvent.php?eventID=\(eventID)&operator=I"
     }
     
-    print(stringURL)
+//    print(stringURL)
+    
+    let Url = URL(string: stringURL)
+    if let url = Url {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: request) {data, response, error in
+            //        let requestReply = NSString(data: data!, encoding: String.Encoding.ascii.rawValue)
+            //        print("Request reply: \(requestReply!)")
+            if error != nil
+            {
+                print("Error in request URL")
+            }
+            }.resume()
+    }
+}
+
+
+func changeFlagCountUser(userID: Int, option: Character) {
+    var stringURL = ""
+    if option == "D"
+    {
+        stringURL = "https://gymbuddyapp.net/flagCountUser.php?userID=\(userID)&operator=D"
+    }
+    else  if option == "I"
+    {
+        stringURL = "https://gymbuddyapp.net/flagCountUser.php?userID=\(userID)&operator=I"
+    }
+    
+//    print(stringURL)
     
     let Url = URL(string: stringURL)
     if let url = Url {
